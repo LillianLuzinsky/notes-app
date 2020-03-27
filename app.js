9ca728bcbbad91ff82d491f7fa0a7f79329d5df4
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-27 18:13:57 +1100

    playing with chalk

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const validator = require('validator')
+// const validator = require('validator')
+const chalk = require("chalk");
+console.log(chalk.green.bold("Success"));
+console.log(chalk.black.bgWhiteBright.bold('this is fun'));
+console.log(chalk.red.bgWhiteBright.inverse("this is fun"));
+
 const getNotes = require('./notes')
 
 const newNotes = getNotes()
 console.log(newNotes)
 
-console.log(validator.isEmail('example.com')) //false
-console.log(validator.isURL('www./hi.com')); //false
+// console.log(validator.isEmail('example.com')) //false
+// console.log(validator.isURL('www./hi.com')); //false
