a072afc3a8a23e67abe40b731807fef10d2a938d
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-27 18:04:36 +1100

    reinstalled npm

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const getNotes = require('./notes')
 const newNotes = getNotes()
 console.log(newNotes)
 
-console.log(validator.isEmail('example.com'))
-console.log(validator.isURL('www./hi.com'));
+console.log(validator.isEmail('example.com')) //false
+console.log(validator.isURL('www./hi.com')); //false
