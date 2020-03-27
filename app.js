f08726d980671573e4fa32d00f1e6d066844d52e
Lillian Luzinsky <eyeris@tpg.com.au>
2020-03-27 17:55:17 +1100

    testing validator.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
+const validator = require('validator')
 const getNotes = require('./notes')
 
 const newNotes = getNotes()
-
 console.log(newNotes)
+
+console.log(validator.isEmail('andrew@example.com'))
